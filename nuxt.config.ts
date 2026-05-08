19e6195219b324f29fab87600701c57d53fd3750
mattyatea <mattyacocacora0@gmail.com>
2026-05-08 17:16:56 +0900

    Enhance: BMCウィジェットを全ページで読み込む

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,20 @@ export default defineNuxtConfig({
           content: "Uptime monitoring powered by Cloudflare Workers",
         },
       ],
+      script: [
+        {
+          src: "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js",
+          "data-name": "BMC-Widget",
+          "data-cfasync": "false",
+          "data-id": "mattyatea",
+          "data-description": "Support me on Buy me a coffee!",
+          "data-message": "",
+          "data-color": "#40DCA5",
+          "data-position": "Right",
+          "data-x_margin": "18",
+          "data-y_margin": "18",
+        },
+      ],
     },
   },
 
